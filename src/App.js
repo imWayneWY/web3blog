5f385b777eff7989010144e6bee416bcfbbe1faf
Yan Wei <im.wayne.wy@gmail.com>
2022-09-02 10:43:55 -0700

    add some deps for project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Grommet, Sidebar, Avatar, Button, Nav, Box } from "grommet";
+import * as Icons from "grommet-icons";
+
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px',
+    },
+  },
+};
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet theme={theme} style={{height: "100vh"}}>
+      <Box direction="row" height={{ min: '100%' }}>
+      <Sidebar background="brand" round="small"
+        header={
+          <Avatar src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80" />
+        }
+        footer={
+          <Button icon={<Icons.Help />} hoverIndicator />
+        }
+      >
+        <Nav gap="small">
+          <Button icon={<Icons.Projects />} hoverIndicator />
+          <Button icon={<Icons.Clock />} hoverIndicator />
+        </Nav>
+      </Sidebar>
+      </Box>
+    </Grommet>
   );
 }
 
