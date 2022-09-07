4df07067fd7525554ecd8f94ca4df06be4dbff1d
Yan Wei <im.wayne.wy@gmail.com>
2022-09-07 16:16:53 -0700

    clean up and try to deploy git page

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Post } from "./pages/Post";
 import { List } from "./pages/List";
 import { Todo } from "./pages/ui-challenges/Todo";
 import { memo } from "react";
 
 export const Router = memo(() => {
-	return <BrowserRouter>
+	return <HashRouter>
 		<Routes>
 			<Route path="/" element={<List items={[]} />} />
 			<Route path="/post" element={<Post />} />
 			<Route path="/todo" element={<Todo />} />
 		</Routes>
-	</BrowserRouter>
+	</HashRouter>
 });
--- src/pages/List.js
+++ src/pages/List.js
@@ -1,6 +1,7 @@
 import { memo } from "react";
 import { Troubleshoot } from "grommet-icons";
 import { Box, Paragraph } from "grommet";
+import { Link } from "react-router-dom";
 
 export const List = memo((items) => {
 	return <>
@@ -9,7 +10,8 @@ export const List = memo((items) => {
 				? <></>
 				: <Box width='100%' align="center" justify="center">
 					<Troubleshoot size="large" />
-					<Paragraph>Nothing here yet...</Paragraph>
+					<Paragraph>Nothing here yet...(Still building)</Paragraph>
+					<Link to="/todo" >todo (a little web3 demo for fun)</Link>
 				</Box>
 		}</>;
 });
