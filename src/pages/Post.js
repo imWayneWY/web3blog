dfab58acfb48ab922e523461d242c08e48c35137
Yan Wei <im.wayne.wy@gmail.com>
2022-09-11 16:45:18 -0700

    add createBlog func

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,13 +1,17 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import MDEditor from '@uiw/react-md-editor';
-import { Box, Button } from "grommet";
+import { Box, Button, TextInput } from "grommet";
 import styled from "styled-components";
 import { create as ipfsHttpClient } from 'ipfs-http-client';
 import { useWallet } from "../hooks/useWallet";
 import { Buffer } from 'buffer';
+import BlogAbi from "../abi/Web3Blog.json";
+import { ethers } from "ethers";
+
+const BlogAddress = "0x2FC33901Ee4c5A790C2Ef7977B5cDc562CF6a045";
 
 const projectId = process.env.REACT_APP_PROJECT_ID;
-const projectSecret = process.env.REACT_APP_PROJECT_ID;
+const projectSecret = process.env.REACT_APP_PROJECT_SECRET;
 
 const auth =
     'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
@@ -20,6 +24,7 @@ const client = ipfsHttpClient({
 		authorization: auth,
 	},
 });
+
 const PostButton = styled(Button)`
 	position: fixed;
 	bottom: 40px;
@@ -27,24 +32,42 @@ const PostButton = styled(Button)`
 `;
 
 export const Post = () => {
-	const { selectedAddress } = useWallet();
+	const { selectedAddress, signer } = useWallet();
 	const [value, setValue] = useState("**Hello world!!!**");
+	const [contract, setContract] = useState();
+	const [title, setTitle] = useState("");
+
+	useEffect(() => {
+		if (!signer) return;
+		const BlogContract = new ethers.Contract(
+			BlogAddress,
+			BlogAbi.abi,
+			signer
+		)
+		setContract(BlogContract);
+	}, [signer]);
+
+	const handleUpdate = useCallback((e) => {
+		setTitle(e.target.value);
+	}, [])
+
 	const handlePost = useCallback(async (e) => {
 		e.preventDefault();
-		if (value.trim() === "") return;
+		if (value.trim() === "" || title.trim() === "" || !contract || !selectedAddress) return;
 		try {
 			const result = await client.add(JSON.stringify({ author: selectedAddress, value, date: Date.now()}));
-			console.log(result);
-			console.log("uri:", `https://ipfs.infura.io/ipfs/${result.path}`);
+			await contract.createBlog(`https://wei.infura-ipfs.io/ipfs/${result.path}`, ethers.utils.formatBytes32String(title.trim()));
+			// todo: redirect to success or home page
 		} catch(error) {
 			console.error("IPFS upload error!")
 			console.error(error);
 		}
- 	}, [selectedAddress, value]);
+ 	}, [contract, selectedAddress, title, value]);
 	return (
-		<Box width='100%' height='100vh'>
+		<Box width='100%' height='100vh' justify="between">
+			<TextInput placeholder="Input your blog title" onChange={handleUpdate} value={title} />
 			<MDEditor
-				height='100%'
+				height='90%'
 				value={value}
 				onChange={setValue}
 			/>
