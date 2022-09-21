e3e9ff7fea9e50201df09771d854aef2ab44d402
Yan Wei <im.wayne.wy@gmail.com>
2022-09-20 20:02:59 -0700

    simple ui polish on calendar

--- src/pages/ui-challenges/Calendar.js
+++ src/pages/ui-challenges/Calendar.js
@@ -2,20 +2,42 @@ import { memo, useEffect, useState } from "react";
 import styled, { css } from "styled-components";
 import {Box} from "grommet";
 
+const MonthHeader = styled.h2`
+	text-align: center;
+`;
+
 const MonthGrid = styled.div`
 	display: grid;
 	grid-template-columns: repeat(7, 1fr);
   	grid-template-rows: repeat(6, 1fr);
+	grid-gap: 6px;
+`;
+
+const HeaderItem = styled.div`
+	height: 50px;
+	line-height: 50px;
+	text-align: center;
+	background: grey;
 `;
 const MonthItem = styled.div`
 	${props => props.$day !== -1 && css`
-		grid-column-start: ${props => props.$day + 1}	
+		grid-column-start: ${props => props.$day + 1};
 	`}
+	${props => props.$isCurrentDay && css`
+		background: green;
+	`}
+	border: 2px grey solid;
+	border-radius: 10px;
+	text-align: center;
+	height: 40px;
+	line-height: 40px;
+	cursor: pointer;
 `;
 
 const WeekHeader = styled.div`
 	display: grid;
 	grid-template-columns: repeat(7, 1fr);
+	margin: 20px 0;
 `;
 
 const MONTHS = [
@@ -43,22 +65,29 @@ const WEEKS = [
 	"SAT",
 ]
 
-const MonthView = memo(({fulldays, month}) => {
+const MonthView = memo(({fulldays, month, currentDay, setCurrentDay}) => {
 	return <>
-		<div>{MONTHS[month]}</div>
+		<MonthHeader>{MONTHS[month]}</MonthHeader>
 		<WeekHeader>
-			{WEEKS.map(w => (<p key={w}>{w}</p>))}
+			{WEEKS.map(w => (<HeaderItem key={w}>{w}</HeaderItem>))}
 		</WeekHeader>
 		<MonthGrid>
 			{fulldays.map(
-				(day, idx) => (<MonthItem key={day} $day={idx === 0 ? day.getDay() : -1}>{day.getDate()}</MonthItem>)
+				(day, idx) => (<MonthItem
+					key={day}
+					$day={idx === 0 ? day.getDay() : -1}
+					$isCurrentDay={currentDay.getDate()===day.getDate()}
+					onClick={() => setCurrentDay(day)}
+				>
+					{day.getDate()}
+				</MonthItem>)
 			)}
 		</MonthGrid>
 	</>;
 });
 
 export const Calendar = memo(() => {
-	const [currentDay, setCurrentDay] = useState(Date.now());
+	const [currentDay, setCurrentDay] = useState(new Date(Date.now()));
 	const [fullDays, setFullDays] = useState([]); // full days of a month
 	const [isMonthView, setIsMonthView] = useState(true);
 
@@ -66,15 +95,14 @@ export const Calendar = memo(() => {
 		setFullDays(getFullDays(currentDay));
 	}, [currentDay]);
 
-	return <Box flex="grow">
-		<MonthView fulldays={fullDays} month={new Date(currentDay).getMonth()} />
+	return <Box flex="grow" pad="medium">
+		<MonthView fulldays={fullDays} month={currentDay.getMonth()} currentDay={currentDay} setCurrentDay={setCurrentDay}/>
 	</Box>;
 });
 
 function getFullDays(currentDay) {
-	const currentDate = new Date(currentDay);
-	const fullyear = currentDate.getFullYear();
-	const month = currentDate.getMonth();
+	const fullyear = currentDay.getFullYear();
+	const month = currentDay.getMonth();
 
 	const dates = new Date(fullyear, month + 1, 0).getDate();
 	const fulldays = [];
