a85aea0b7ac042a5d94d367808ef72060d107821
Yan Wei <im.wayne.wy@gmail.com>
2022-10-08 12:33:58 -0700

    simple calc height

--- src/pages/ui-challenges/Giphy.js
+++ src/pages/ui-challenges/Giphy.js
@@ -9,19 +9,29 @@ const Wrapper = styled.div`
 
 const Column = styled.div`
 	flex: 1;
+	position: relative;
+`;
+
+const Image = styled.img`
+	position: absolute;
+	left: 10px;
+	width: calc(100% - 20px);
+	top: ${props => props.top}px;
 `;
 const URL = "https://picsum.photos/v2/list";
 const amount = 50;
 
 export const Giphy = memo(() => {
 	const [images, setImages] = useState([]);
+	const [col1Images, setCol1Images] = useState([]);
+	const [col2Images, setCol2Images] = useState([]);
+	const [col3Images, setCol3Images] = useState([]);
 	const [wrapperRef, setWrapperRef] = useState();
+	const [columnWidth, setColumnWidth] = useState(0);
 
 	useLayoutEffect(() => {
 		if (!wrapperRef) return;
-		const columnWidth = wrapperRef.clientWidth / 2;
-		console.log(columnWidth);
-
+		setColumnWidth(wrapperRef.clientWidth / 3 - 20);
 	}, [wrapperRef])
 
 	const fetchImages = useCallback((page) => {
@@ -35,9 +45,26 @@ export const Giphy = memo(() => {
 		fetchImages(1);
 	}, [fetchImages]);
 
+	useEffect(() => {
+		console.warn(columnWidth)
+		if (!images.length || !columnWidth) return;
+		console.log(images[0]);
+		console.log(columnWidth);
+		
+		const img1height = columnWidth / (images[0].width / images[0].height);
+		console.warn(img1height);
+		const top2 = img1height + 10;
+		setCol1Images([{...images[0], top: 0}, {...images[1], top: top2}]);
+
+	}, [columnWidth, images]);
+
 	return <Wrapper ref={setWrapperRef}>
-		<Column />
-		<Column />
-		<Column />
+		<Column id="col0">
+			{
+				col1Images.map(img => <Image src ={img.download_url} top={img.top}/>)
+			}
+		</Column>
+		<Column id="col1" />
+		<Column id="col2" />
 	</Wrapper>
 });
