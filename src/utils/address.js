9b9a3523284fa80f0370fe38053b8dd729bbd6e1
Yan Wei <im.wayne.wy@gmail.com>
2022-11-03 17:50:20 -0700

    update address

--- src/utils/address.js
+++ src/utils/address.js
@@ -1,6 +1,6 @@
-export const BlogAddress = "0x1f9aeebbbE56E27AAf13220C76e5dF207e9e85Bc";
+export const BlogAddress = "0x32f27275BbB9E9Acf868A03C7a8EF051aCfcE708";
 export const TasksContractAddress = "0xF6E430c546b1DF82Eb524984DFeb7bb60396615e";
 export const CalendarContractAddress = "0x6f11E94524DaFD6ac607c838CC3393376A56Ca96";
-export const GuestBlogAddress = "0x9b78CFcED998f12F1A51325137E32c264c194e41";
+export const GuestBlogAddress = "0xB12303fFBA6465C3f6b38c6be33747Cc951a05A9";
 export const JiraContractAddress = "0x5079E2448812cfD92A9Ee23858901541041AaB39";
 export const CommentContractAddress = "0x886e67b5a4aE58b15f405C0CfCE4E46c64ac5486";
